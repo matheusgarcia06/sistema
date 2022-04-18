e210d649f33ddf36279b2863b0f7aafb9d354fee
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2022-04-18 15:01:03 -0300

    feat: Adicionando a feature alertas com react-toastify na pagina de login e logout

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,5 +1,6 @@
 import { useState, createContext, useEffect } from 'react';
 import firebase from '../services/firebaseConnection';
+import { toast } from 'react-toastify';
 
 export const AuthContext = createContext('');
 
@@ -48,9 +49,11 @@ function AuthProvider({ children }) {
 				setUser(data);
 				storageUser(data);
 				setLoadingAuth(false);
+				toast.success('Bem vindo de volta!!');
 			})
 			.catch((error) => {
 				console.log(error);
+				toast.error('Ops algo deu errado');
 				setLoading(false);
 			});
 	}
@@ -82,10 +85,12 @@ function AuthProvider({ children }) {
 						setUser(data);
 						storageUser(data);
 						setLoadingAuth(false);
+						toast.success('Bem vindo a plataforma!');
 					})
 
 					.catch((error) => {
 						console.log(error);
+						toast.error('Ops algo deu errado');
 						setLoadingAuth(false);
 					});
 			});
