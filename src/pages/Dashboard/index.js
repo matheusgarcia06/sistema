e6ba79c4ce66f1c2c039f952bd5d93df5581dac6
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2022-04-19 17:00:23 -0300

    feat: adicionando componente header na página dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,15 @@
 import { useContext } from 'react';
 import { AuthContext } from '../../contexts/auth';
 
+import Header from '../../components/Header';
+
 export default function Dashboard() {
 	const { signOut } = useContext(AuthContext);
 
 	return (
 		<div>
+			<Header />
+
 			<h1>Página DashBoard</h1>
 			<button onClick={() => signOut()}>Fazer logout</button>
 		</div>
